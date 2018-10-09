048058f4780d6983d1f6da6545f18cfd2c314639
Tristan <tri510@hotmail.com>
2018-10-09 22:20:55 +1100

    updates

--- Javascript/projChart.js
+++ Javascript/projChart.js
@@ -28,14 +28,3 @@ var chart = new Chart(ctx, {
     options: {}
 });
 
-var mymap = L.map('mapid').setView([-33.8688, 151.2093], 14);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidHJpc3RhbnczIiwiYSI6ImNqbWN1ZmNvaTB5dWIzcHE0OXF1bWhzZm0ifQ.bjH3NyqIaJ0sTfyZ-vRaAA', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
-
-
-L.geoJSON(lgaData).addTo(mymap);
