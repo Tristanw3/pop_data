861909f29359900ff5caa79efa84e121a8047b25
Tristan <tri510@hotmail.com>
2018-11-30 21:43:15 +1100

    update1

--- Javascript/projLGAMap.js
+++ Javascript/projLGAMap.js
@@ -1,23 +1,57 @@
-var mymap = L.map('mapid').setView([-33.8688, 151.2093], 14);
+// selFeat.feature.properties.LGA_NAME16
+
+var selFeat = {};
+var myMap = L.map('mapid').setView([-33.8688, 151.2093], 14);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidHJpc3RhbnczIiwiYSI6ImNqbWN1ZmNvaTB5dWIzcHE0OXF1bWhzZm0ifQ.bjH3NyqIaJ0sTfyZ-vRaAA', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: 'mapbox.dark',
     accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+}).addTo(myMap);
 
 
-L.geoJSON(lgaData).addTo(mymap);
+function highlightFeature(e) {
+    resetHighlight(selFeat)
+    var layer = e.target;
 
-// function getLGAName(click) {
-//     console.log(click)
-//     // console.log(lgaData.features[0].properties.Name)
-// };
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
 
-mymap.on('click', onMapClick);
-
-function onMapClick(e) {
-    alert("You clicked the map at " + e.target);
-    console.log(e.target);
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    selFeat = e.target;
 }
+
+
+function resetHighlight(e) {
+    mapper.resetStyle(e);
+}
+
+function zoomToFeature(e) {
+    myMap.fitBounds(e.target.getBounds());
+}
+
+function runner(e) {
+    highlightFeature(e);
+    zoomToFeature(e);
+
+}
+
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        click: runner
+    });
+}
+
+mapper = L.geoJSON(lgaData, {
+    fillColor: '#328F32',
+    color: 'white',
+    onEachFeature: onEachFeature
+}).addTo(myMap);
